e6fcf84d90357099c18e321b18bf0bc8d5739f03
Alex Martin <alex@subtleco.co>
2021-01-12 14:48:27 -0600

    Added FishDataProvider module, imported to main.js, and linked main script to index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+import { useFish } from './FishDataProvider.js'
+
 console.log("Welcome to the main module")
+
+const allTheFish = useFish()
+
+for (const fish of allTheFish) {
+    console.log(fish)
+}
