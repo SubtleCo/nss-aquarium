383b6e0541bd593091d0063c1cfb045e1b0eb01c
Alex Martin <alex@subtleco.co>
2021-01-13 14:40:47 -0600

    cleaned up import statements of main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
-import { useFish } from './fish/FishDataProvider.js';
 import { FishList } from './fish/FishList.js';
-import { useTip } from './tips/TipDataProvider.js';
 import { TipList } from './tips/TipList.js';
-import { useLocation } from './locations/LocationDataProvider.js';
 import { LocationList } from './locations/LocationList.js';
 
 console.log("Welcome to the main module")
